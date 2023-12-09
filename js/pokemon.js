87a3c7084b13736c31200b7b99b50240f839cf26
HECTOR <hevonox@gmail.com>
2023-12-09 00:31:02 -0600

    iniciamos la peticion http a la PokeAPI

--- js/pokemon.js
+++ js/pokemon.js
@@ -6,3 +6,9 @@ const notFoundMessage = document.querySelector('#not-found-message');
 
 let allPokemons = [];
 
+fetch(`https://pokeapi.co/api/v2/pokemon?limit=${MAX_POKEMON}`)
+.then((response) => response.json())
+.then((data) =>{
+
+    allPokemons = data.results;
+})
