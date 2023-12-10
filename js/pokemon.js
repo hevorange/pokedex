71eb90158d4f7ceb57feb268d4ff1612b53e9840
HECTOR <hevonox@gmail.com>
2023-12-10 01:06:58 -0600

    realizamos las peticiones al API para cargar los datos de los pokemons en el HTML

--- js/pokemon.js
+++ js/pokemon.js
@@ -11,4 +11,65 @@ fetch(`https://pokeapi.co/api/v2/pokemon?limit=${MAX_POKEMON}`)
 .then((data) =>{
 
     allPokemons = data.results;
-})
+});
+
+const fetchPokemonDataBeforeRedirect= async (id)=>{
+
+    try {
+
+        const [pokemon, pokemonSpecies] = await Promise
+        .all([fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((res)=>
+            res.json()
+        ),
+    
+        fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`).then((res)=>
+            res.json()
+        ),
+    ]);
+
+        return true;
+
+    } catch (error) {
+
+        console.log('Petición fallida para obtener los pokemon, porfavor vuelve a recargar la página');
+
+    }
+
+};
+
+
+const displayPokemons = (pokemon)=>{
+
+    listWrapper.innerHTML="";
+    pokemon.forEach((pokemon) =>{
+
+        const pokemonID= pokemon.url.split("/")[6];
+        const listItem = document.createElement("div");
+        listItem.className= "list-item";
+        listItem.innerHTML=`
+        
+            <div class="number-wrap">
+            
+                <p class="caption-fonts">#${pokemonID}</p>
+            </div>
+
+            <div class="img-wrap">
+            
+                <img src="https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/other/dream-world/${pokemonID}.svg" alt="${pokemon.name}" />
+            </div>
+
+            <div class="name-wrap">
+            
+                <p class="body3-fonts">#${pokemon.name}</p>
+            </div>
+        `;
+
+        listItem.addEventListener('click', async ()=>{
+
+
+            const success = await fetchPokemonDataBeforeRedirect(pokemonID);
+
+        });
+    })
+
+};
